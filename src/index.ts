003af0216efc2b1220326add026c738bb3dda03c
afisicos <afisicos@gmail.com>
2025-11-18 23:20:08 +0100

    greet

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend funcionando 🚀");
+});
+
+
 // Endpoint: crear partida
 app.post("/create", (req, res) => {
   const game = createGame();
